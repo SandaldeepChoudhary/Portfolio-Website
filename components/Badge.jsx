5b803e0f081e94d32fb333ba5098c2328a1db697
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-03-10 14:39:45 +0530

    Badge Added

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -14,7 +14,7 @@ const Badge = ({
       <div className="text-3xl text-primary">{icon}</div>
       <div className="flex items-center gap-x-2">
         <div className="text-4xl leading-none font-bold text-primary">
-          <CountUp end={endCountNum} delay={1} duration={4} />
+          <CountUp end={endCountNum} delay={1} duration={4} />{endCountText}
         </div>
         <div className="max-w-[70px] leading-none text-[15px] font-medium text-black">
           {badgeText}
