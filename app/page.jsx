a6188fd5630fa24f7065c44d10f3eb13dcb86767
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-03-10 15:10:31 +0530

    Selection and Scrollbar Added

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 //components
+import About from "@/components/About";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero/>
+      <About/>
     </main>
   );
 }
--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,7 +1,29 @@
+import { AlignJustify } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Logo from "./Logo";
+import Nav from "./Nav";
+import Socials from "./Socials";
+
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
-  )
-}
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col items-center justify-between h-full py-8">
+          <div className="flex flex-col items-center gap-y-32">
+            <Logo />
+            <Nav
+              containerStyles="flex flex-col items-center gap-y-6"
+              linkStyles="text-2xl"
+            />
+          </div>
+          <Socials containerStyles="flex gap-x-4" linkStyles="text-2xl" />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
