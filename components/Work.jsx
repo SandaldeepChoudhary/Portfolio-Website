be20a3a2d42679857286303c31b0da18ded1e11c
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-03-10 23:51:30 +0530

    Project Section Added

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,7 +1,103 @@
+"use client";
+
+//import swipper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+//import required modules
+import { Pagination } from "swiper/modules";
+
+//componet
+import ProjectCard from "./ProjectCard";
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+//import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const projectData = [
+  {
+    image: "/work/1.png",
+    category: "fullstack",
+    name: "Income Expense App",
+    description: "loremsad sadfa sdd sadf sgsdfg addg d gdfg df dfgd dfg d",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "next js",
+    name: "Portfolio Website",
+    description: "loremsad sadfa sdd sadf sgsdfg addg d gdfg df dfgd dfg d",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "next js",
+    name: "User Authentication System",
+    description: "loremsad sadfa sdd sadf sgsdfg addg d gdfg df dfgd dfg d",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "react js",
+    name: "Profile Website",
+    description: "loremsad sadfa sdd sadf sgsdfg addg d gdfg df dfgd dfg d",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "react js",
+    name: "Bubble Game",
+    description: "loremsad sadfa sdd sadf sgsdfg addg d gdfg df dfgd dfg d",
+    link: "/",
+    github: "/",
+  },
+];
 const Work = () => {
   return (
-    <div>Work</div>
-  )
-}
+    <section className="relative mb-12 xl:mb-48">
+      <div className="container mx-auto">
+        {/* text  */}
+        <div className="max-x-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
+          <h2 className="section-title mb-4">Latest Projects</h2>
+          <p className="subtitle mb-8">
+            Lorem, ipsum doler dsf shsha hasdfa sdfsdf.
+          </p>
+          <Link href="/projects">
+            <Button>All projects</Button>
+          </Link>
+        </div>
+        {/* slider  */}
+        <div className="xl:max-w-[1000px] xl:absolute right-0 top-0">
+          <Swiper
+            className="h-[480px]"
+            slidesPerView={1}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+            }}
+            spaceBetween={30}
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+          >
+            {/* show only the first 4 projects for the slides  */}
+            {projectData.slice(0, 4).map((project, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <ProjectCard project={project} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Work
+export default Work;
