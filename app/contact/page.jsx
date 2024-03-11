5592f5d3272d56ffabead5375b950502bcd3ab5a
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-03-11 20:57:27 +0530

    Contact Page Updated

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
               Say Hello 👋
             </div>
             <h1 className="h1 max-w-md mb-8">Let's Work Together.</h1>
-            <p className="subtitle max-w-[400px]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate, laudantium!</p>
+            <p className="subtitle max-w-[400px]">Feel free to drop me a message below or reach out using the contact information provided. I look forward to hearing from you!</p>
           </div>
           {/* illustration  */}
           <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat">
