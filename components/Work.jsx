76bc2e250a16f196631268c772a62beaa82edad6
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-03-11 16:25:37 +0530

    Update Work Section

--- components/Work.jsx
+++ components/Work.jsx
@@ -70,7 +70,7 @@ const Work = () => {
         {/* text  */}
         <div className="max-x-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
           <h2 className="section-title mb-4">Latest Projects</h2>
-          <p className="subtitle mb-8 xl:w-[400px]">
+          <p className="subtitle mb-8 lg:w-[400px]">
             Explore my projects showcasing web development skills in creating
             dynamic and responsive websites with modern designs and
             functionalities
