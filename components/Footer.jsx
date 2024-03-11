9df970a8691b0a841c137ad443ca30830d38f6ef
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-03-11 11:42:22 +0530

    Footer Added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,6 +1,17 @@
+import Socials from "./Socials"
+
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-secondary py-12"><div className="container mx-auto">
+      <div className="flex flex-col items-center justify-between">
+        {/* socials  */}
+        <Socials containerStyles=" flex gap-x-6 mx-auto xl:mx-0 mb-4" iconsStyles="text-primary dark:text-white/70 text-[20px] hover:text-black dark:hover:text-primary transition-all "/>
+        {/* copyright  */}
+        <div className="text-muted-foreground">
+          Copyright &copy; Sandaldeep. All rights reserved.
+        </div>
+        </div></div></footer>
   )
 }
 
