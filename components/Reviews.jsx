81ac86dd7605f9a49095e68b6ef550b054be3ac9
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-03-12 16:00:41 +0530

    Reviews Updated

--- components/Reviews.jsx
+++ components/Reviews.jsx
@@ -28,7 +28,7 @@ const reviewsData = [
       "As an individual, Sandaldeep is reliable and professional. His communication skills make collaboration effortless.",
   },
   {
-    avatar: "/reviews/Sudarshana.jpg",
+    avatar: "/reviews/Sudarshana.jpeg",
     name: "Sudarshana Thakur",
     job: "Software Developer",
     review:
