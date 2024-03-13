a1e26404406c9ac5a16eff813c6e93c96e92e9cf
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-03-13 10:09:38 +0530

    Form Service Added

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,7 @@
 import Form from "@/components/Form";
 import { HomeIcon, MailIcon, PhoneCall } from "lucide-react";
 
+
 const Contact = () => {
   return (
     <section>
@@ -14,12 +15,13 @@ const Contact = () => {
               Say Hello 👋
             </div>
             <h1 className="h1 max-w-md mb-8">Let's Work Together.</h1>
-            <p className="subtitle max-w-[400px]">Feel free to drop me a message below or reach out using the contact information provided. I look forward to hearing from you!</p>
+            <p className="subtitle max-w-[400px]">
+              Feel free to drop me a message below or reach out using the
+              contact information provided. I look forward to hearing from you!
+            </p>
           </div>
           {/* illustration  */}
-          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat">
-
-          </div>
+          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat"></div>
         </div>
         {/* info text & form  */}
         <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
@@ -27,18 +29,21 @@ const Contact = () => {
           <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
             {/* mail  */}
             <div className="flex items-center gap-x-8">
-              <MailIcon size={18} className="text-primary"/> <div>sandaldeep@outlook.in</div>
+              <MailIcon size={18} className="text-primary" />{" "}
+              <div>sandaldeep@outlook.in</div>
             </div>
             {/* address  */}
             <div className="flex items-center gap-x-8">
-              <HomeIcon size={18} className="text-primary"/> <div>Palampur, Himachal Pradesh, India</div>
+              <HomeIcon size={18} className="text-primary" />{" "}
+              <div>Palampur, Himachal Pradesh, India</div>
             </div>
             {/* phone  */}
             <div className="flex items-center gap-x-8">
-              <PhoneCall size={18} className="text-primary"/> <div>+91-6230023316</div>
+              <PhoneCall size={18} className="text-primary" />{" "}
+              <div>+91-6230023316</div>
             </div>
           </div>
-          <Form/>
+          <Form />
         </div>
       </div>
     </section>
--- components/Form.jsx
+++ components/Form.jsx
@@ -5,10 +5,19 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
 
+//form service
+import { useForm, ValidationError } from "@formspree/react";
+
+
+
 
 const Form = () => {
+  const [state, handleSubmit] = useForm("mkndzybe");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
-    <form className="flex flex-col gap-y-4">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-y-4">
         {/* input  */}
         <div className="relative flex items-center">
             <Input type="name" id="name" placeholder="Name"/>
@@ -24,7 +33,7 @@ const Form = () => {
             <Textarea placeholder="Type Your Message Here..."/>
             <MessageSquare className="absolute right-6" size={20}/>
         </div>
-        <Button className="flex items-center gap-x-1 max-w-[166px]">Let's Talk
+        <Button type ="submit" disabled={state.submitting} className="flex items-center gap-x-1 max-w-[166px]">Let's Talk
         <ArrowRightIcon size={20}/></Button>
     </form>
   )
